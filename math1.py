47ac38fa5b6c1a97d17ad6ff9313e906f853cbd6
nilmani-tiwari <sambadnilmani085@gmail.com>
2021-06-06 17:24:43 +0530

    Add files via upload

---
+++ math1.py
@@ -0,0 +1,21 @@
+def add(x,y):
+    return x+y
+    
+def sub(x,y):
+    return x-y
+    
+def square(x):
+    return x*x
+    
+def squar(x,y):
+    result=x**2+y**2
+    return result
+def div(x,y):
+    return x/y
+def multyplication(x,y):
+    return x*y
+
+  
+def cube(x):
+    return x**3
+        
